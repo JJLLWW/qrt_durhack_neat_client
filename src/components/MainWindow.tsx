211fe787112a08180df5bdf4c8495392984a913d
Jack Wright <ugh@ugh.com>
2024-01-04 11:38:56 +0000

    ui skeleton for file view tab

--- src/components/MainWindow.tsx
+++ src/components/MainWindow.tsx
@@ -1,6 +1,7 @@
-import { Button, Table, Select, Flex, Typography, Modal } from "antd";
+import { Button, Table, Select, Flex, Typography, Modal, Input, Space } from "antd";
 import React, { useState } from "react";
 import type { ColumnsType} from "antd/es/table";
+import { SearchOutlined } from '@ant-design/icons';
 
 const { Title } = Typography;
 
@@ -24,7 +25,7 @@ function get_val(i: number): DataType {
 
 const data: DataType[] = Array(50).fill({}).map((_: any, idx: number) => { return get_val(idx)})
 
-// how would highlighting work? <- antd filters work with a set of mutually exclusive options
+// how would highlighting work? <- react-highlight-words
 // only want regex search on the message, some kind of time interval for timestamp?
 // filterDropdown?
 // store the filter state in some object?
@@ -44,7 +45,34 @@ export default function MainWindow() {
             title: 'Timestamp',
             dataIndex: 'timestamp',
             key: 'timestamp',
-            width: '20%'
+            width: '20%',
+            filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => {
+                return <div style={{padding: 8}}>
+                    <Input placeholder="TIME_FROM" style={{marginBottom: 8}} />
+                    <Input placeholder="TIME_TO" style={{marginBottom: 8}} />
+                    <Space>
+                        <Button
+                            type="primary"
+                            onClick={() => {confirm()}}
+                            icon={<SearchOutlined />}
+                            size="small"
+                            style={{ width: 90 }}
+                        >
+                            Search
+                        </Button>
+                        <Button
+                            onClick={() => {clearFilters && clearFilters(); setSelectedKeys([])}}
+                            size="small"
+                            style={{ width: 90 }}
+                        >
+                            Reset
+                        </Button>
+                    </Space>
+                </div>
+            },
+            onFilter: (value: any, record: DataType) => {
+                return true;
+            }
         },
         {
             title: 'Status',
@@ -63,6 +91,37 @@ export default function MainWindow() {
             title: 'Message',
             dataIndex: 'message',
             key: 'message',
+            filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => {
+                return <div style={{ padding: 8 }}>
+                    <Input
+                        placeholder={'Filter By Regex'}
+                        value={selectedKeys[0]}
+                        onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+                        style={{marginBottom: 8}}
+                    />
+                    <Space>
+                        <Button
+                            type="primary"
+                            onClick={() => {confirm()}}
+                            icon={<SearchOutlined />}
+                            size="small"
+                            style={{ width: 90 }}
+                        >
+                            Search
+                        </Button>
+                        <Button
+                            onClick={() => {clearFilters && clearFilters(); setSelectedKeys([])}}
+                            size="small"
+                            style={{ width: 90 }}
+                        >
+                            Reset
+                        </Button>
+                    </Space>
+                </div>
+            },
+            onFilter: (value: any, record: DataType) => {
+                return record.message.includes(value)
+            }
         },
         {
             title: 'Stats',
@@ -92,7 +151,7 @@ export default function MainWindow() {
     return (
         <>
             <Flex vertical={true}>
-                <Title>File View</Title>
+                <Title level={2}>File View</Title>
                 <Select
                     placeholder={"Choose Log File"}
                     onChange={(value: string) => {}}
@@ -100,12 +159,14 @@ export default function MainWindow() {
                         {value: "A", label: "A"},
                         {value: "B", label: "B"}
                     ]}
+                    style={{marginBottom: 8}}
                 />
                 <Table
                     dataSource={data}
                     columns={columns}
                     pagination={false}
                     scroll={{y: 600}}
+                    style={{marginBottom: 8}}
                 />
                 <Modal
                     title="Stats Info at 24/5/23 13:20:45.213789"
@@ -116,13 +177,16 @@ export default function MainWindow() {
                 >
                     <Table dataSource={data} columns={columns} />
                 </Modal>
-                <Flex vertical={false}>
+                <Space>
                     <Button type="primary">SAVE FILTER STATE</Button>
                     <Select placeholder="CHOOSE SAVED FILTER" options={[
                         {value: "A", label: "A"},
                         {value: "B", label: "B"}]
                     }/>
-                </Flex>
+                    <Input
+                        placeholder="HIGHLIGHT BY REGEX"
+                    />
+                </Space>
             </Flex>
         </>
     )
